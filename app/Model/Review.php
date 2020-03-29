5cb0d5e3d4b5f301c6cbbd51ce9480af82df7bfb
vikash keswani <vikashkeswani@gmail.com>
2020-03-29 12:47:02 -0700

    migration created for product and reviews

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     //
+
+    public function user()
+{
+    return $this->belongsTo('App\Model\Product', 'product_id', 'id');
+}
 }
